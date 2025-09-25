b6420eddd690e9bb12b379e7d73a7ad156e0cda5
enrich-t <enrichlaunch@gmail.com>
2025-09-25 10:50:23 -0600

    Delete api/api directory

--- api/api/hello.ts
+++
@@ -1,5 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-export default function handler(_req: VercelRequest, res: VercelResponse) {
-  res.setHeader('Content-Type','application/json');
-  res.status(200).send(JSON.stringify({ ok: true, runtime: 'node' }));
-}
